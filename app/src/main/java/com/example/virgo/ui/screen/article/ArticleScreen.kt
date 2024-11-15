b4529ea3cc3afafc427afd641229cd33bce73c42
ThiNg041 <a2k57tp@gmail.com>
2024-11-15 21:06:45 +0700

    Finish ArticleUI and FacilityUI

--- app/src/main/java/com/example/virgo/ui/screen/article/ArticleScreen.kt
+++ app/src/main/java/com/example/virgo/ui/screen/article/ArticleScreen.kt
@@ -1,2 +1,76 @@
 package com.example.virgo.ui.screen.article
 
+import android.annotation.SuppressLint
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun ArticleContent(url: String) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.White
+                ),
+                title = { /* Title can be added here if needed */ }
+            )
+        },
+        content = { paddingValues ->
+            // WebView to display the article content
+            AndroidView(
+                factory = {
+                    WebView(it).apply {
+                        layoutParams = ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT
+                        )
+
+                        webViewClient = WebViewClient()
+
+                        settings.javaScriptEnabled = true
+                        loadUrl(url)
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                update = {
+                    it.loadUrl(url)
+                }
+            )
+        }
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ArticleContentPreview() {
+    ArticleContent(url = "file:///android_asset/article.html",)
+}
